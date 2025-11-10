2527dc4ec99026e774be160dac627fe61d52d1b7
ARISTIDE-CMD <aristidegaelkouandjakenfack@gmail.com>
2025-11-10 21:17:53 +0100

    correction de la table user

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema; 
 use App\Models\User; // Ajoutez cette ligne pour importer le modèle User
 use App\Models\Project;
 use App\Observers\ProjectObserver;
